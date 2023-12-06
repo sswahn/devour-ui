840b6728b7a97d989440c41e6bac36b2ecdc6942
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-12-06 14:41:39 -0500

    Update Avatar.js

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import UserIcon from './UserIcon.js'
+import UserIcon from '../Icons/UserIcon.js'
 import styles from './styles.module.css'
 
 const Avatar = ({ className, image, username, onClick, size }) => {
