a8fde1e0a33dced8a98e6ad2b4007785c56057f4
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-12-06 12:31:13 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import Register from './components/Register/Register.js'
 import Login from './components/Login/Login.js'
 import ForgotPassword from './components/ForgotPassword/ForgotPassword.js'
 import ConfirmForgotPassword from './components/ConfirmForgotPassword/ConfirmForgotPassword.js'
+import Checkbox from './components/Checkbox/Checkbox.js'
+import Chat from './components/Chat/Chat.js'
 
 export {
   Register,
   Login,
   ForgotPassword,
-  ConfirmForgotPassword
+  ConfirmForgotPassword,
+  Checkbox,
+  Chat
 }
