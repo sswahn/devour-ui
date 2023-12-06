12b3cc2be0c54b62632053f05236270c4fff55f7
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-12-06 12:38:19 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import Chat from './components/Chat/Chat.js'
 import Checkbox from './components/Checkbox/Checkbox.js'
 import Dropdown from './components/Dropdown/Dropdown.js'
 
+import Modal from './components/Modal/Modal.js'
+
 export {
   Chat,
   Checkbox,
   Dropdown,
+
+  Modal,
 }
