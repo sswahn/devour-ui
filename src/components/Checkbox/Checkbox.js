e411bc2e286266e10772565a86d032881f9d6698
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-12-06 12:26:57 -0500

    Create Checkbox.js

---
+++ src/components/Checkbox/Checkbox.js
@@ -0,0 +1,22 @@
+import { useRef, memo } from 'react'
+import CheckedIcon from '../Icons/CheckedIcon.js'
+import UncheckedIcon from '../Icons/UncheckedIcon.js'
+import styles from './styles.module.css'
+
+const Checkbox = memo(({ className, label, checked, onChange }) => {
+  const checkboxRef = useRef(null)
+
+  const handleClick = event => {
+    checkboxRef.current.click()
+  }
+  
+  return (
+    <div className={`${styles.container} ${className || ''}`}>
+      <label className={styles.label} onClick={handleClick}>{label}</label>
+      <input className={styles.checkbox} ref={checkboxRef} type="checkbox" checked={checked} onChange={onChange} />
+      {checked ? <CheckedIcon className={styles.icon} onClick={handleClick} /> : <UncheckedIcon className={styles.icon} onClick={handleClick} />}
+    </div>
+  )
+})
+
+export default Checkbox
