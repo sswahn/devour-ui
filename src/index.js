ce3e0ab08213facab264c7fa0283798de0cb95ef
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-12-06 13:00:19 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Chat from './components/Chat/Chat.js'
 import Checkbox from './components/Checkbox/Checkbox.js'
 import Dropdown from './components/Dropdown/Dropdown.js'
-
+import Login from './components/Login/Login.js'
 import Modal from './components/Modal/Modal.js'
 import Search from './components/Search/Search.js'
 import Sidebar from './components/Sidebar/Sidebar.js'
@@ -11,7 +11,7 @@ export {
   Chat,
   Checkbox,
   Dropdown,
-
+  Login,
   Modal,
   Search,
   Sidebar,
